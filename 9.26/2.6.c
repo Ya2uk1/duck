947ec3955ea5bbff6917d5a0e959a28f69d74fab
Ya2uk1 <15535378921@163.com>
2026-09-26 22:57:04 +0800

    第二章第六题

---
+++ 9.26/2.6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+	int toes;
+	toes = 10;
+	//定义变量toes为10
+	printf("The variable toes = %d.\n",toes);
+	//打印toes的值
+	printf("double toes = %d.\n",2*toes);
+	//计算并打印两倍toes的值
+	printf("toes's square = %d.\n",toes*toes);
+	//计算并打印toes的平方的值
+	return 0;
+}
